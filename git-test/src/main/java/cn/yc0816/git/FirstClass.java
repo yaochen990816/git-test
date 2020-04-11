2389f1a355c5157f335570f83ba5e24c74224fef
yaochen990816 <63460220+yaochen990816@users.noreply.github.com>
2020-04-11 14:34:55 +0800

    吧本地测试工程添加到git

---
+++ git-test/src/main/java/cn/yc0816/git/FirstClass.java
@@ -0,0 +1,14 @@
+package cn.yc0816.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+    }
+}
